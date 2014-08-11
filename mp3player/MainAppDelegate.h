ab02edf85eccb37df1a8005dcf7203cd03c6c995
Rahul Morade <rahul@maxdigi.com>
2014-08-11 12:55:00 +0530

    Initial Commit

---
+++ mp3player/MainAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MainAppDelegate.h
+//  mp3player
+//
+//  Created by Rahul on 11/08/14.
+//  Copyright (c) 2014 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ mp3player/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  mp3player
+//
+//  Created by Rahul on 11/08/14.
+//  Copyright (c) 2014 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MainAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MainAppDelegate class]));
+    }
+}
